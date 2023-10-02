9acaa0db2d1bd191507bd5d874ae9f00f5c7a0ba
Dipu Haider <dipuhaider@gmail.com>
2023-10-02 09:45:14 +0600

    Project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "theme-primary": "#FF444A",
+        "theme-dark": "#0B0B0B",
+        "theme-txt40": "#0B0B0B66",
+        "theme-txt50": "#0B0B0B80",
+        "theme-txt70": "#0B0B0BB2",
+        "theme-btn-green": "#009444",
+        "theme-status-green": "#00C49F",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
