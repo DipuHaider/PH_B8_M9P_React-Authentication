853591b54a5e6467da79cac83cb9f7c3cd29f651
Dipu Haider <dipuhaider@gmail.com>
2023-10-02 12:33:55 +0600

    login and registration page design

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,60 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const name = e.target.email.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password)
+    }
+
+
     return (
-        <div>
+        <div className="hero min-h-screen bg-base-200">
             <Helmet>
                 <title>RA | Register</title>
             </Helmet>
-            <h2>Register</h2>
+            <div className="hero-content flex-col">
+            <div className="text-center">
+                <h1 className="text-5xl font-bold">Register now!</h1>
+                <p className="py-6">Register Authentication with firebase by Email And Password</p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <form onSubmit={handleRegister}>
+                        <div className="form-control">
+                            <label className="label">
+                            <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" name="name" required placeholder="Your name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                            <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" required placeholder="Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                            <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" required placeholder="Password" className="input input-bordered" />
+                            <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-emerald-300 text-white">Register</button>
+                        </div>
+                    </form>
+                    <p>Already Registered? <Link to="/login"><button className="btn bg-cyan-400 btn-sm text-white">Login</button></Link></p>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
